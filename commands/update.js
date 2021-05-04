b2d15e620d96748f33b3ae0e61a544985e6fed31
SushyDev <xsushy@hotmail.com>
2021-05-04 15:59:24 +0200

    better error handling for api queries

--- commands/update.js
+++ commands/update.js
@@ -24,9 +24,28 @@ module.exports = {
         // ? Instanciate user
         const user = new User(message, args, data, this);
 
+        // ? Instanciate user
+        const user = new User(message, args, data, this);
+
+        const fetchMojang = async (username) => {
+            try {
+                return await user.fetchMojang(username);
+            } catch (error) {
+                errorMessage(message, error);
+            }
+        };
+
+        const fetchHypixel = async (UUID) => {
+            try {
+                await user.fetchHypixel(UUID);
+            } catch (error) {
+                errorMessage(message, error);
+            }
+        };
+
         // ? Fetch Api data
-        const mojangData = await user.fetchMojang(args[0]);
-        const hypixelData = await user.fetchHypixel(mojangData.id);
+        const mojangData = await fetchMojang(args[0]);
+        const hypixelData = await fetchHypixel(mojangData.id);
 
         const player = hypixelData.player;
         const member = message.member;
